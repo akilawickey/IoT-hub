e801aebad085515e3805bbdb1e5b38a561162c77
Akila Wickey <asak999@gmail.com>
2016-12-26 10:27:49 +0530

    new google sheet data uploading code soil moisture

--- esp8266_node.ino
+++ esp8266_node.ino
@@ -1,112 +1,98 @@
-/*
- *  HTTPS with follow-redirect example
- *  Created by Sujay S. Phadke, 2016
- *  
- *  Based on the WifiClientSecure example by
- *  Ivan Grokhotkov
- *  *
- *  This example is in public domain.
- */
 
 #include <ESP8266WiFi.h>
 #include "HTTPSRedirect.h"
-#include "DHT.h"
-#define DHTPIN D0
 
-#define DHTTYPE DHT22 
-
-// Replace with your network credentials
 const char* ssid = "ZTE";
 const char* password = "12345678912340000000000000";
 
+// The ID below comes from Google Sheets.
+// Towards the bottom of this page, it will explain how this can be obtained
+const char *GScriptId = "AKfycby42E0NLno0D6L66odVyMFm9YTBw2LhWE0uyabWRgPpNE3PhJSj";
+// web app url https://script.google.com/macros/s/AKfycby42E0NLno0D6L66odVyMFm9YTBw2LhWE0uyabWRgPpNE3PhJSj/exec
+// Push data on this interval
+const int dataPostDelay = 4000;  // 15 minutes = 15 * 60 * 1000
+
 const char* host = "script.google.com";
 const char* googleRedirHost = "script.googleusercontent.com";
-const char *GScriptId = "AKfycbxIyRVLnWZVv4yog8JUkQcyk7jByufQTM5wVmFtreKjrBjGbXI";
 
+const int httpsPort =  443;
+HTTPSRedirect client(httpsPort);
 
-const int httpsPort = 443;
+// Prepare the url (without the varying data)
+String url = String("/macros/s/") + GScriptId + "/exec?";
 
-// http://askubuntu.com/questions/156620/how-to-verify-the-ssl-fingerprint-by-command-line-wget-curl/
-// echo | openssl s_client -connect script.google.com:443 |& openssl x509 -fingerprint -noout
-// www.grc.com doesn't seem to get the right fingerprint
-// SHA1 fingerprint of the certificate
-//const char* fingerprint = "94 2F 19 F7 A8 B4 5B 09 90 34 36 B2 2A C4 7F 17 06 AC 6A 2E";
 const char* fingerprint = "F0 5C 74 77 3F 6B 25 D7 3B 66 4D 43 2F 7E BC 5B E9 28 86 AD";
-const char* fingerprint2 = "94 64 D8 75 DE 5D 3A E6 3B A7 B6 15 52 72 CC 51 7A BA 2B BE";
-DHT dht(DHTPIN, DHTTYPE);
 
-void setup()
-{
+// We will take analog input from A0 pin 
+const int AnalogIn     = A0; 
+
+void setup() {
   Serial.begin(115200);
-  dht.begin();
-  Serial.print("Connecting to " + *ssid);
+  Serial.println("Connecting to wifi: ");
+  Serial.println(ssid);
+  Serial.flush();
+
   WiFi.begin(ssid, password);
-  Serial.println("going into wl connect");
-  while (WiFi.status() != WL_CONNECTED) //not connected,..waiting to connect
-  {
-    delay(1000);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
     Serial.print(".");
   }
-  Serial.println("wl connected");
-  Serial.println("");
-  Serial.println("Credentials accepted! Connected to wifi\n ");
-  Serial.println("");
-}
-void loop()
-{
-  float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  float f = dht.readTemperature(true);
+  Serial.println(" IP address: ");
+  Serial.println(WiFi.localIP());
+
   
-//  float Humidity = DHT.humidity;
-//  float Temperature = DHT.temperature;
-  String url = String("/macros/s/") + GScriptId + String("/exec?Temperature=") + t + String("&Humidity=") + h;
-  if (isnan(h) || isnan(t))
-  {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
-  }
-  Serial.print("Humidity: ");
-  Serial.print(h);
-  Serial.print(" %\t");
-  Serial.print("Temperature in Cel: ");
-  Serial.print(t);
-  Serial.print(" *C ");
-  HTTPSRedirect client(httpsPort);
-  Serial.print("Connecting to ");
+  Serial.print(String("Connecting to "));
   Serial.println(host);
+
   bool flag = false;
-  for (int i = 0; i < 5; i++) {
+  for (int i=0; i<5; i++){
     int retval = client.connect(host, httpsPort);
     if (retval == 1) {
-      flag = true;
-      break;
+       flag = true;
+       break;
     }
     else
       Serial.println("Connection failed. Retrying...");
   }
+
+  // Connection Status, 1 = Connected, 0 is not.
+  Serial.println("Connection Status: " + String(client.connected()));
   Serial.flush();
-  if (!flag) {
+  
+  if (!flag){
     Serial.print("Could not connect to server: ");
     Serial.println(host);
     Serial.println("Exiting...");
+    Serial.flush();
     return;
   }
-  Serial.flush();
+
+  // Data will still be pushed even certification don't match.
   if (client.verify(fingerprint, host)) {
     Serial.println("Certificate match.");
   } else {
     Serial.println("Certificate mis-match");
   }
-  client.printRedir(url, host, googleRedirHost);
-  Serial.println("==============================================================================");
-  {
-    HTTPSRedirect client(httpsPort);
-  }
-  if (!client.connected())
+}
+
+// This is the main method where data gets pushed to the Google sheet
+void postData(String tag, float value){
+  if (!client.connected()){
+    Serial.println("Connecting to client again..."); 
     client.connect(host, httpsPort);
-  Serial.println("closing connection");
-  delay(1000);                       // Send data every 1 hour
+  }
+  String urlFinal = url + "tag=" + tag + "&value=" + String(value);
+  client.printRedir(urlFinal, host, googleRedirHost);
+}
+
+// Continue pushing data at a given interval
+void loop() {
+  
+  // Read analog value, in this case a soil moisture
+  int data = 1023 - analogRead(AnalogIn);
+
+  // Post these information
+  postData("SoilMoisture", data);
+  
+  delay (dataPostDelay);
 }
